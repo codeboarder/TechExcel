b4844d01fca6445c8b72f382d509203251f82a07
Rick Weyenberg <rickwey@microsoft.com>
2025-01-17 10:46:45 -0600

    RMW-SQLServer Changes

--- src/data/AddManagedIdentityUser.sql
+++ src/data/AddManagedIdentityUser.sql
@@ -1,4 +1,4 @@
-CREATE USER TechExcelMI FROM EXTERNAL PROVIDER;
-ALTER ROLE db_datareader ADD MEMBER TechExcelMI;
-ALTER ROLE db_datawriter ADD MEMBER TechExcelMI;
+CREATE USER id-fnooc6hrt57hq FROM EXTERNAL PROVIDER;
+ALTER ROLE db_datareader ADD MEMBER id-fnooc6hrt57hq;
+ALTER ROLE db_datawriter ADD MEMBER id-fnooc6hrt57hq;
 
